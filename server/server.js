ec1583d915bbf6c15adb2d1d36f0530522b59bcf
ablecrew <teddy.dande4@gmail.com>
2026-04-19 23:44:06 +0300

    updated URLs

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ const app = express();
 
 // 🔐 Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5178",
+  origin: process.env.FRONTEND_URL,
   credentials: true,
 }));
 
